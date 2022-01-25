a133e15bace4aafbe385cca5c368ff6dbfb9a44b
Trawwade <edahmitchel@gmail.com>
2022-01-25 12:18:27 +0100

    buit funcions

---
+++ drum.js
@@ -0,0 +1,25 @@
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    // console.log(audio)
+    if(!audio) return
+  audio.currentTime = 0
+  audio.play()
+    key.classList.add("playing")
+  
+  // console.log(key)
+
+  }
+  function removeTransition (e){;2
+    if (e.propertyName !== 'transform') return;
+    console.log(this)
+    this.classList.remove('playing')
+  
+
+  }
+  
+   
+  const keys= document.querySelectorAll('.key')
+  keys.forEach(key => key.addEventListener("transitionend", removeTransition))
+ 
+  window.onkeydown=playSound
